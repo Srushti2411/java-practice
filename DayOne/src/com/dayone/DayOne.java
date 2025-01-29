d263a4522ec20ea8cd970218997be4ecc65d80cd
Srushti2411 <srushtigarad24@gmail.com>
2025-01-29 13:47:12 +0530

    day one

---
+++ DayOne/src/com/dayone/DayOne.java
@@ -0,0 +1,11 @@
+package com.dayone;
+
+public class DayOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First Day Of Java");
+
+	}
+
+}
---
+++ DayOne/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author SRUSHTI
+ *
+ */
+module DayOne {
+}
